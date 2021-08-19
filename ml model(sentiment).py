ff3ccedcc25c4935663ffef83528c6518cd6768f
Vidhi Jain <73524453+vidhi4532@users.noreply.github.com>
2021-08-19 13:48:16 +0530

    Add files via upload

--- ml model(sentiment).py
+++ ml model(sentiment).py
@@ -11,11 +11,16 @@ from sklearn.linear_model import Perceptron
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 from sklearn.metrics import confusion_matrix,accuracy_score,precision_score,recall_score
 from sklearn.model_selection import cross_validate, KFold
 import datetime
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
 import time
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+
 
 data=pd.read_csv("filesss.csv",index_col=False)
 #print(data)
@@ -44,59 +49,20 @@ SEED=4
 X=data.full_text
 y=data.sent_score
 
-X_train,X_val_test,y_train,y_val_test=train_test_split(X,y,test_size=0.1,random_state=SEED)
-X_val,X_test,y_val,y_test=train_test_split(X_val_test,y_val_test,test_size=0.5,random_state=SEED)
-
+#x_train,x_val_test,y_train,y_val_test = train_test_split(X,y,test_size=0.1,random_state=SEED)
+#x_val,x_test,y_val,y_test = train_test_split(x_val_test,y_val_test,test_size=0.5,random_state=SEED)
 #print(len(X_train),len(X_val_test),len(X_val))
+#print(x_traintf)
 
-classifiers=[MultinomialNB(),BernoulliNB(),AdaBoostClassifier(),
-RidgeClassifier(),PassiveAggressiveClassifier(),Perceptron(),RandomForestClassifier()]
+pipeline = Pipeline([
+    ('bow',CountVectorizer(analyzer='word')),  # strings to token integer counts
+    ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
+    ('classifier',LinearSVC()),  # train on TF-IDF vectors w/ Naive Bayes classifier
+])
 
-
-clf_names=['MultinomialNB()','BernoulliNB()','AdaBoostClassifier()',
-'RidgeClassifier()','PassiveAggressiveClassifier()','Perceptron()','RandomForest Classifier()']
-
-df=[]
-
-for v in ['cv','tf']:
-    for gram in range(1,4):
-        i=0
-
-        for clf in classifiers:
-            
-            if(clf=='RandomForest Classifier'):#special case
-                clf = RandomForestClassifier(random_state=0,n_jobs=-1,class_weight="balanced")
-
-            before = datetime.datetime.now()
-            before = before.strftime("%H:%M:%S")
-            start = time.time()
-            
-            if(v=='cv'):
-                vec = TfidfVectorizer(ngram_range=(1,gram))
-            else:
-                vec = CountVectorizer(ngram_range=(1,gram))
-                
-            model = make_pipeline(vec,clf)
-            model.fit(X_train.values.astype('U'),y_train.values.astype('U'))##
-            labels = model.predict(X_val.values.astype('U'))
-            ac = accuracy_score(y_val.values.astype('U'),labels)
-            kfold = KFold(n_splits=10,shuffle=False,random_state=None)
-            results = cross_validate(model,X_train.values.astype('U'),y_train.values.astype('U'),cv=kfold,return_train_score=True)
-            crossval_test_score_mean=results['test_score'].mean()
-            crossval_train_score_mean=results['train_score'].mean()
-            crossval_test_score_std=results['test_score'].std()
-            crossval_train_score_std=results['train_score'].std()
-            after = datetime.datetime.now()
-            after = after.strftime("%H:%M:%S")
-            end = time.time()
-            hours = int(after[0:2])-int(before[0:2])
-            mins = int(after[3:5])-int(before[3:5])
-            secs = int(after[6:8])-int(before[6:8])
-            time_taken = str(hours)+":"+str(mins)+":"+str(secs)
-            gr = str(gram)
-            vec_gram = v+"_"+gr
-            df.append([vec_gram,clf_names[i],ac,crossval_train_score_mean,crossval_test_score_mean,crossval_train_score_std,crossval_test_score_std, end-start])
-            i+=1
-            # data.append([0,0,0,0,0,0,0,0])
-d = pd.DataFrame(df,columns=['Vec_Gram','Classifier','Ac','crossval_train_score_mean','crossval_test_score_mean','crossval_train_score_std','crossval_test_score_std','Time.2'])
-print(d)
+msg_train, msg_test, label_train, label_test=train_test_split(data['full_text'].values.astype('U'), data['sent_score'].values.astype('U'), test_size=0.2)
+pipeline.fit(msg_train,label_train)
+predictions = pipeline.predict(msg_test)
+print(classification_report(predictions,label_test))
+print(confusion_matrix(predictions,label_test))
+print(accuracy_score(predictions,label_test))
